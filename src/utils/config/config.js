68eeb198d7cb2f01c803b137980be6760ecb960e
Fabien <fabien.hupel@gmail.com>
2021-09-30 11:24:11 +0200

    change on 404 page parameter

--- src/utils/config/config.js
+++ src/utils/config/config.js
@@ -4,4 +4,4 @@ export const apiUrl =
         : 'https://argentbank-backend.herokuapp.com/api/v1'
 
 export const routerBaseName =
-    process.env.NODE_ENV === 'development' ? '/' : 'Fabienhupel_13_10092021/'
+    process.env.NODE_ENV === 'development' ? '/' : '/Fabienhupel_13_10092021'
