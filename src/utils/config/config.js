efc8990d014564959c7799f6051adecc63c80b71
Fabien <fabien.hupel@gmail.com>
2021-09-30 11:11:55 +0200

    change config file

--- src/utils/config/config.js
+++ src/utils/config/config.js
@@ -4,6 +4,4 @@ export const apiUrl =
         : 'https://argentbank-backend.herokuapp.com/api/v1'
 
 export const routerBaseName =
-    process.env.NODE_ENV === 'development'
-        ? '/'
-        : 'https://fabhup.github.io/Fabienhupel_13_10092021/'
+    process.env.NODE_ENV === 'development' ? '/' : 'Fabienhupel_13_10092021/'
